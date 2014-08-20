5e61ad6bc0d8cc94bc20e6aaba90e463ab252146
David <hichaeretaqua@web.de>
2014-08-20 17:41:18 +0200

    Added EventHandlerExtension.

---
+++ Tanpohp.Extensions/EventHandlerExtension.cs
@@ -0,0 +1,21 @@
+﻿#region usings
+
+using System;
+
+#endregion
+
+namespace Tanpohp.Extensions
+{
+    public static class EventHandlerExtension
+    {
+        /// <summary>
+        /// Invokes handler with EventArgs.Empty after checking if listeners exists.
+        /// </summary>
+        /// <param name="handler">EventHandler to invoke.</param>
+        /// <param name="sender">Sender that invokes event. Usually "this".</param>
+        public static void CheckedInvoke(this EventHandler handler, object sender)
+        {
+            if (handler != null) handler(sender, EventArgs.Empty);
+        }
+    }
+}
