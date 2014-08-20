7c51c5d090d00053a6768efd6947ee55c9e547c6
David <hichaeretaqua@web.de>
2014-08-20 17:27:23 +0200

    Added Solution and first class with test.

---
+++ Tanpohp.ExtensionTest/Class1.cs
@@ -0,0 +1,49 @@
+﻿#region usings
+
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class DoubleExtensionTest
+    {
+        [Test]
+        public void TestInRange1()
+        {
+            const int lower = 1;
+            const int upper = 2;
+            const double value = 2.5;
+
+            var inRange = value.InRange(lower, upper);
+
+            Assert.AreEqual(false, inRange);
+        }
+
+        [Test]
+        public void TestInRange2()
+        {
+            const int lower = 1;
+            const int upper = 2;
+            const double value = 1.5;
+
+            var inRange = value.InRange(lower, upper);
+
+            Assert.AreEqual(true, inRange);
+        }
+
+        [Test]
+        public void TestInRange3()
+        {
+            const int lower = -1;
+            const int upper = 2;
+            const double value = 1.5;
+
+            var inRange = value.InRange(lower, upper);
+
+            Assert.AreEqual(true, inRange);
+        }
+    }
+}
---
+++ Tanpohp.Extensions/DoubleExtension.cs
@@ -0,0 +1,24 @@
+﻿#region usings
+
+using System;
+
+#endregion
+
+namespace Tanpohp.Extensions
+{
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Determines whether value is in between a specific range defined by [lower, upper].
+        /// </summary>
+        /// <param name="value">Value to check.</param>
+        /// <param name="lower">Lower border of range.</param>
+        /// <param name="upper">Upper order of range.</param>
+        /// <returns></returns>
+        public static bool InRange(this double value, double lower, double upper)
+        {
+            if (lower > upper) throw new ArgumentException("Lower has to be lower or equals to upper.");
+            return value.CompareTo(lower) != -1 && value.CompareTo(upper) != 1;
+        }
+    }
+}
