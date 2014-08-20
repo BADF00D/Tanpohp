8fc7966c29e1ea5fc988814caf783609991904df
David <hichaeretaqua@web.de>
2014-08-20 17:46:20 +0200

    Added ListExtension and tests.

---
+++ Tanpohp.ExtensionTest/ListExtensionTest.cs
@@ -0,0 +1,27 @@
+﻿#region usings
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class ListExtensionTest
+    {
+        [Test]
+        public void TestAddRange1()
+        {
+            IList<int> sequence = new List<int>();
+
+            sequence.AddRange(new[] {1, 2, 3});
+
+            Assert.AreEqual(3, sequence.Count);
+            Assert.IsTrue(sequence.Contains(1));
+            Assert.IsTrue(sequence.Contains(2));
+            Assert.IsTrue(sequence.Contains(3));
+        }
+    }
+}
---
+++ Tanpohp.Extensions/ListExtension.cs
@@ -0,0 +1,17 @@
+﻿#region usings
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace Tanpohp.Extensions
+{
+    public static class ListExtension
+    {
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> values)
+        {
+            if (list is List<T>) (list as List<T>).AddRange(values);
+            else values.ForEach(list.Add);
+        }
+    }
+}
