9e9cc2250acd932bd2219bae330d1ed3e0d7435d
David <hichaeretaqua@web.de>
2014-08-20 18:05:15 +0200

    Added StringExtension and test.

---
+++ Tanpohp.ExtensionTest/StringExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿#region usings
+
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        public void TestFormatWith()
+        {
+            var expected = string.Format("{0}", "Test");
+            var actual = "{0}".FormatWith("Test");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tanpohp.Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace Tanpohp.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Simply wrapps string.Format method.
+        /// </summary>
+        /// <param name="format">String with format.</param>
+        /// <param name="parameter">Arrya with parameter.</param>
+        /// <returns></returns>
+        //[StringFormatMethod("format")] //todo
+        public static string FormatWith(this string format, params object[] parameter)
+        {
+            return string.Format(format, parameter);
+        }
+    }
+}
