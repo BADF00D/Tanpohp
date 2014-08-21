4c26b3f01f3f8797e5d05ba7bb28a78351f11b01
David <hichaeretaqua@web.de>
2014-08-21 15:33:27 +0200

    Added NumberExtension ands test.

---
+++ Tanpohp.ExtensionTest/NumberExtensionTest.cs
@@ -0,0 +1,163 @@
+﻿#region usings
+
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class NumberExtensionTest
+    {
+        [Test]
+        public void TestIsEvenWithShort1()
+        {
+            Assert.IsTrue(((short)2).IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithShort2()
+        {
+            Assert.IsFalse(((short)3).IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithShort3()
+        {
+            Assert.IsTrue(((short)0).IsEven());
+        }
+
+        [Test]
+        public void TestIsEvenWithInt1()
+        {
+            Assert.IsTrue((2).IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithInt2()
+        {
+            Assert.IsFalse((3).IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithInt3()
+        {
+            Assert.IsTrue((0).IsEven());
+        }
+
+        [Test]
+        public void TestIsEvenWithLong1()
+        {
+            Assert.IsTrue(2L.IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithLong2()
+        {
+            Assert.IsFalse(3L.IsEven());
+        }
+        [Test]
+        public void TestIsEvenWithLong3()
+        {
+            Assert.IsTrue(0L.IsEven());
+        }
+
+        [Test]
+        public void TestIsOddWithShort1()
+        {
+            Assert.IsFalse(((short)2).IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithShort2()
+        {
+            Assert.IsTrue(((short)3).IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithShort3()
+        {
+            Assert.IsFalse(((short)0).IsOdd());
+        }
+
+        [Test]
+        public void TestIsOddWithInt1()
+        {
+            Assert.IsFalse((2).IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithInt2()
+        {
+            Assert.IsTrue((3).IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithInt3()
+        {
+            Assert.IsFalse((0).IsOdd());
+        }
+
+        [Test]
+        public void TestIsOddWithLong1()
+        {
+            Assert.IsFalse(2L.IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithLong2()
+        {
+            Assert.IsTrue(3L.IsOdd());
+        }
+        [Test]
+        public void TestIsOddWithLong3()
+        {
+            Assert.IsFalse(0L.IsOdd());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithShort16()
+        {
+            Assert.IsTrue(((short)16).IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithShort15()
+        {
+            Assert.IsFalse(((short)15).IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithShortZero()
+        {
+            Assert.IsTrue(((short)0).IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithInt16()
+        {
+            Assert.IsTrue(16.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithInt15()
+        {
+            Assert.IsFalse(15.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithIntZero()
+        {
+            Assert.IsTrue(0.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithLong16()
+        {
+            Assert.IsTrue(16L.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithLong15()
+        {
+            Assert.IsFalse(15L.IsPowerOfTwo());
+        }
+
+        [Test]
+        public void TestIsPowerOfTwoWithLongZero()
+        {
+            Assert.IsTrue(0L.IsPowerOfTwo());
+        }
+    }
+}
---
+++ Tanpohp.Extensions/NumberExtension.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Tanpohp.Extensions
+{
+    public static class NumberExtension
+    {
+        /// <summary>
+        /// Determines if given value is odd number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is odd, false otherwise.</returns>
+        public static bool IsOdd(this Int32 value)
+        {
+            return value % 2 == 1;
+        }
+
+        /// <summary>
+        /// Determines if given value is odd number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is odd, false otherwise.</returns>
+        public static bool IsOdd(this Int16 value)
+        {
+            return value % 2 == 1;
+        }
+
+        /// <summary>
+        /// Determines if given value is odd number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is odd, false otherwise.</returns>
+        public static bool IsOdd(this Int64 value)
+        {
+            return value % 2 == 1;
+        }
+
+        /// <summary>
+        /// Determines if given value is even number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsEven(this Int32 value)
+        {
+            return value % 2 == 0;
+        }
+
+        /// <summary>
+        /// Determines if given value is even number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsEven(this Int16 value)
+        {
+            return value % 2 == 0;
+        }
+
+        /// <summary>
+        /// Determines if given value is even number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsEven(this Int64 value)
+        {
+            return value % 2 == 0;
+        }
+
+        /// <summary>
+        /// Determines if given value is power of two or 2^x with x as natural number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsPowerOfTwo(this Int32 value)
+        {
+            var copy = value;
+            return ((copy >> 1) << 1) == value;
+        }
+
+        /// <summary>
+        /// Determines if given value is power of two or 2^x with x as natural number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsPowerOfTwo(this Int16 value)
+        {
+            var copy = value;
+            return ((copy >> 1) << 1) == value;
+        }
+
+        /// <summary>
+        /// Determines if given value is power of two or 2^x with x as natural number.
+        /// </summary>
+        /// <param name="value">Value to evaluate.</param>
+        /// <returns>True if number is even, false otherwise.</returns>
+        public static bool IsPowerOfTwo(this Int64 value)
+        {
+            var copy = value;
+            return ((copy >> 1) << 1) == value;
+        }
+    }
+}
