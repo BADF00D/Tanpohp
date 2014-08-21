485a41378d198af52bf5e1c8bd9077ad884834e6
David <hichaeretaqua@web.de>
2014-08-21 14:19:48 +0200

    Added some more extension methods for IEnumerable<T> and tests.

--- Tanpohp.ExtensionTest/EnumerableExtensionTest.cs
+++ Tanpohp.ExtensionTest/EnumerableExtensionTest.cs
@@ -1,5 +1,7 @@
 ﻿#region usings
 
+using System;
+using System.Linq;
 using NUnit.Framework;
 using Tanpohp.Extensions;
 
@@ -11,13 +13,263 @@ namespace Tanpohp.ExtensionTest
     public class EnumerableExtensionTest
     {
         [Test]
-        public void Test1()
+        public void TestForEach()
         {
             var sequence = new [] {1, 2, 3};
 
             var sum = 0;
             sequence.ForEach(i => sum += i);
             Assert.AreEqual(6, sum);
+        }
+
+        [Test]
+        public void TestContains()
+        {
+            var sequence = new [] {1, 2, 3};
+            
+            Assert.IsTrue(sequence.Contains(i => i==1));
+        }
+
+        [Test]
+        public void TestItemsToString1()
+        {
+            var sequence = new[] { 1, 2, 3 };
+
+            var result = sequence.ItemsToString(i => "" + i, "?");
+
+            Assert.AreEqual("1?2?3", result);
+        }
+
+        [Test]
+        public void TestItemsToString2()
+        {
+            var sequence = new int[0];
+
+            var result = sequence.ItemsToString(i => "" + i, "?");
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void TestItemsToString3()
+        {
+            var sequence = new[] { 1, 2, 3 };
+
+            var result = sequence.ItemsToString("x");
+
+            Assert.AreEqual("1x2x3", result);
+        }
+
+        [Test]
+        public void TestItemsToString4()
+        {
+            var sequence = new int [0];
+
+            var result = sequence.ItemsToString("x");
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void TestToHashSet1()
+        {
+            var sequence = new[] { 1, 2, 3 };
+
+            var set = sequence.ToHashSet();
+
+            Assert.IsTrue(set.Contains(1));
+            Assert.IsTrue(set.Contains(2));
+            Assert.IsTrue(set.Contains(3));
+            Assert.AreEqual(3, set.Count);
+        }
+
+        [Test]
+        public void TestToHashSet2()
+        {
+            var sequence = new[] { 1, 2, 3,1 };
+
+            var set = sequence.ToHashSet();
+
+            Assert.IsTrue(set.Contains(1));
+            Assert.IsTrue(set.Contains(2));
+            Assert.IsTrue(set.Contains(3));
+            Assert.AreEqual(3, set.Count);
+        }
+
+        [Test]
+        public void TestToDictionaryGroupedBy()
+        {
+            var sequence = new[]
+                               {
+                                   new Dummy("Paul", 23), new Dummy("Pauline", 22), new Dummy("Eric", 20),
+                                   new Dummy("Berta", 23)
+                               };
+
+            var personGroupedByAge = sequence.ToDictionaryGroupedBy(person => person.Age);
+
+            Assert.AreEqual(3, personGroupedByAge.Count);
+            Assert.AreEqual(2, personGroupedByAge[23].Count);
+            Assert.AreEqual(1, personGroupedByAge[22].Count);
+            Assert.AreEqual(1, personGroupedByAge[20].Count);
+        }
+
+        [Test]
+        public void TestContainsOnlyUniqueItems1()
+        {
+            var sequence = new[]
+                               {
+                                   new Dummy("Paul", 23), new Dummy("Pauline", 22), new Dummy("Eric", 20),
+                                   new Dummy("Berta", 23)
+                               };
+
+            Assert.IsTrue(sequence.ContainsOnlyUniqueItems());
+        }
+
+        [Test]
+        public void TestContainsOnlyUniqueItems2()
+        {
+            var sequence = new[]
+                               {
+                                   new Dummy("Paul", 23), new Dummy("Pauline", 22), new Dummy("Eric", 20),
+                                   new Dummy("Paul", 23)
+                               };
+
+            Assert.IsFalse(sequence.ContainsOnlyUniqueItems());
+        }
+
+        [Test]
+        public void TestTakeFirst1()
+        {
+            var sequence = new[] {1, 2, 3, 4, 5, 6};
+
+            var items = sequence.TakeFirst(2).ToList();
+            Assert.AreEqual(2, items.Count());
+            Assert.AreEqual(1, items[0]);
+            Assert.AreEqual(2, items[1]);
+        }
+
+        [Test]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void FailTestTakeFirst2()
+        {
+            var sequence = new[] { 1 };
+
+            sequence.TakeFirst(2).ToList();
+        }
+
+        [Test]
+        public void TestTakeLast1()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var items = sequence.TakeLast(2, true).ToList();
+            Assert.AreEqual(2, items.Count());
+            Assert.AreEqual(5, items[0]);
+            Assert.AreEqual(6, items[1]);
+        }
+
+        [Test]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void FailTestTakeLast()
+        {
+            var sequence = new[] { 1 };
+
+            sequence.TakeLast(2).ToList();
+        }
+
+        [Test]
+        public void TestIsEmpty1()
+        {
+            var sequence = new[] { 1 };
+
+            Assert.IsFalse(sequence.IsEmpty());
+        }
+
+        [Test]
+        public void TestIsEmpty2()
+        {
+            var sequence = new int[0];
+
+            Assert.IsTrue(sequence.IsEmpty());
+        }
+
+        [Test]
+        public void TestItemAt1()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var items = sequence.ItemAt(3);
+            Assert.AreEqual(4, items);
+        }
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailItemAt1()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var items = sequence.ItemAt(-3);
+            Assert.AreEqual(4, items);
+        }
+
+        [Test]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void FailItemAt2()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            var items = sequence.ItemAt(10);
+            Assert.AreEqual(4, items);
+        }
+
+        [Test]
+        public void TestNon1()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            Assert.IsTrue(sequence.Non(i=>i == 10));
+        }
+        [Test]
+        public void TestNon2()
+        {
+            var sequence = new[] { 1, 2, 3, 4, 5, 6 };
+
+            Assert.IsFalse(sequence.Non(i => i == 3));
+        }
+
+        private class Dummy
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public Dummy(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != typeof (Dummy)) return false;
+                return Equals((Dummy) obj);
+            }
+
+            public bool Equals(Dummy other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return Equals(other.Name, Name) && other.Age == Age;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ Age; ;
+                }
+            }
         }
     }
 }
--- Tanpohp.Extensions/EnumerableExtension.cs
+++ Tanpohp.Extensions/EnumerableExtension.cs
@@ -2,6 +2,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using Tanpohp.Annotations.Resharper;
 
 #endregion
@@ -16,12 +19,195 @@ namespace Tanpohp.Extensions
         /// <typeparam name="T">Generic type.</typeparam>
         /// <param name="sequence">Sequence to walk through.</param>
         /// <param name="action">Action to perform on each item.</param>
+        [DebuggerStepThrough]
         public static void ForEach<T>(this IEnumerable<T> sequence, [NotNull]Action<T> action)
         {
             foreach (var item in sequence)
             {
                 action(item);
             }
+        }
+        /// <summary>
+        /// Checks whether source contains a element the condition (evaluator) matches.
+        /// This method is an alias for extension method Any.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="predicate">Predicate that should match.</param>
+        /// <returns>True if there is an item that matches predicate.</returns>
+        [DebuggerStepThrough]
+        public static bool Contains<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            return source.Any(item => predicate(item));
+        }
+
+        /// <summary>
+        /// Converts enumerable into a string by walking over it and calling propertySelector on each item.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="sequence">Sequence to walk through.</param>
+        /// <param name="propertySelector">Function that converts item T into a its string representation.</param>
+        /// <param name="seperator">Seperator between each item in final string.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static string ItemsToString<T>(
+            this IEnumerable<T> sequence, Func<T, string> propertySelector, string seperator = ";")
+        {
+            var stringBuilder = new StringBuilder(200);
+            foreach (var item in sequence)
+            {
+                stringBuilder.Append(propertySelector(item));
+                stringBuilder.Append(seperator);
+            }
+            if (stringBuilder.Length > 0)
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Converts enumerable into a string by walking over it and calling ToString on each item.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="sequence">Sequence to walk through.</param>
+        /// <param name="seperator">Seperator between each item in final string.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static string ItemsToString<T>(this IEnumerable<T> sequence, string seperator = ";")
+        {
+            return ItemsToString(sequence, i => i.ToString(), seperator);
+        }
+
+        /// <summary>
+        /// Converts a enumerable to a HashSet.
+        /// </summary>
+        /// <typeparam name="T">Generic type.</typeparam>
+        /// <param name="source">Source enumeation that should be used.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+        {
+            return new HashSet<T>(source);
+        }
+
+        /// <summary>
+        /// Groups all items from source into a dictionary, where all items with the same key are stored in a list.
+        /// </summary>
+        /// <typeparam name="TKey">Type that should be key in resulting dictionary.</typeparam>
+        /// <typeparam name="TSource">Type of item in source.</typeparam>
+        /// <param name="source">Source collection.</param>
+        /// <param name="keySelector">Selector for the key that is invoked in each item.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static Dictionary<TKey, List<TSource>> ToDictionaryGroupedBy<TKey, TSource>(
+            this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var result = new Dictionary<TKey, List<TSource>>();
+            foreach (var item in source)
+            {
+                var key = keySelector(item);
+                if (result.ContainsKey(key) == false)
+                    result.Add(key, new List<TSource>());
+
+                result[key].Add(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks whether each item only occure one within the collection using the default equality operator to compare values.
+        /// </summary>
+        /// <typeparam name="T">Generic type.</typeparam>
+        /// <param name="source">Source collection.</param>
+        /// <returns>True if collection only contains unique items, false otherwise.</returns>
+        /// <remarks>Remember to correctly overwrite Equals and GetHashCode methods of T</remarks>
+        [DebuggerStepThrough]
+        public static bool ContainsOnlyUniqueItems<T>(this IEnumerable<T> source)
+        {
+            var enumerable = source as IList<T> ?? source.ToList();
+            return enumerable.Distinct().Count() == enumerable.Count();
+        }
+
+        /// <summary>
+        /// Takes the first 'count' elements from the sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the sequence items.</typeparam>
+        /// <param name="sequence">Sequence to take items from.</param>
+        /// <param name="count">Maximum number of items to take start from sequence. If squence contains less then 'count' items, it returns the whole sequence as is.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static IEnumerable<T> TakeFirst<T>(this IEnumerable<T> sequence, int count)
+        {
+            if (count < 0) throw new ArgumentException("Invalid count of elements. Parameter count should be biggern or equals then 0.");
+            foreach (var item in sequence)
+            {
+                if (count-- > 0) yield return item;
+                else break;
+            }
+            if(count > 0) throw new IndexOutOfRangeException("Count is bigger then number of eleemtns in sequence.");
+        }
+
+        /// <summary>
+        /// Takes the last 'count' elements from the sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the sequence items.</typeparam>
+        /// <param name="sequence">Sequence to take items from.</param>
+        /// <param name="count">Maximum number of items to take from end sequence. If squence contains less then 'count' items, it returns the whole sequence as is.</param>
+        /// <param name="preserveOrder">True items have same order as they came in.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> sequence, int count, bool preserveOrder = false)
+        {
+            var result = TakeFirst(sequence.Reverse(), count);
+
+            return preserveOrder ? result.Reverse() : result;
+        }
+
+        /// <summary>
+        /// Alias for !sequence.Any()
+        /// </summary>
+        /// <typeparam name="T">Generic type.</typeparam>
+        /// <param name="sequence">Source sequence.</param>
+        /// <returns>True if source is empty, false otherwise.</returns>
+        [DebuggerStepThrough]
+        public static bool IsEmpty<T>(this IEnumerable<T> sequence)
+        {
+            return !sequence.Any();
+        }
+
+        /// <summary>
+        /// Enumerates through the sequence and return element a specified position.
+        /// </summary>
+        /// <typeparam name="T">Generic type.</typeparam>
+        /// <param name="sequence">Source sequnce.</param>
+        /// <param name="position">Zero based postion of item to return.</param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static T ItemAt<T>(this IEnumerable<T> sequence, int position)
+        {
+            if(position < 0) throw new ArgumentException("Parameter postion must be bigger or equals then 0.");
+            try
+            {
+                return sequence.Skip(position).First();
+            }
+            catch (InvalidOperationException)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Checks if none of the given elements satisfy the condition.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="sequence">Sequence to check.</param>
+        /// <param name="condition">Condition to check.</param>
+        /// <returns>True if no element satisfy the condition, false otherwise.</returns>
+        [DebuggerStepThrough]
+        public static bool Non<T>(this IEnumerable<T> sequence, Func<T, bool> condition )
+        {
+            return !sequence.Any(condition);
         }
     }
 }
