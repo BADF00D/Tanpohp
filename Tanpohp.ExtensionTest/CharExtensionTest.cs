46d946e02a9842979e0531ca28b6e47a975c31c2
David <hichaeretaqua@web.de>
2014-10-04 20:51:23 +0200

    Added CharExtension and its tests.

---
+++ Tanpohp.ExtensionTest/CharExtensionTest.cs
@@ -0,0 +1,59 @@
+﻿#region usings
+
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class CharExtensionTest
+    {
+        [Test]
+        public static void IsBracket()
+        {
+            Assert.IsTrue('('.IsBracket());
+            Assert.IsTrue(')'.IsBracket());
+            Assert.IsTrue('['.IsBracket());
+            Assert.IsTrue(']'.IsBracket());
+            Assert.IsTrue('{'.IsBracket());
+            Assert.IsTrue('}'.IsBracket());
+            for (var i = 0; i < 10; i++)
+                Assert.IsFalse(i.ToString().ToCharArray()[0].IsBracket());
+        }
+
+        [Test]
+        public static void IsCircleBracket()
+        {
+            Assert.IsTrue('('.IsCircleBracket());
+            Assert.IsTrue(')'.IsCircleBracket());
+            Assert.IsFalse('['.IsCircleBracket());
+            Assert.IsFalse(']'.IsCircleBracket());
+            Assert.IsFalse('{'.IsCircleBracket());
+            Assert.IsFalse('}'.IsCircleBracket());
+        }
+
+        [Test]
+        public static void IsCurlyBracket()
+        {
+            Assert.IsFalse('('.IsCurlyBracket());
+            Assert.IsFalse(')'.IsCurlyBracket());
+            Assert.IsFalse('['.IsCurlyBracket());
+            Assert.IsFalse(']'.IsCurlyBracket());
+            Assert.IsTrue('{'.IsCurlyBracket());
+            Assert.IsTrue('}'.IsCurlyBracket());
+        }
+
+        [Test]
+        public static void IsSquareBracket()
+        {
+            Assert.IsFalse('('.IsSquareBracket());
+            Assert.IsFalse(')'.IsSquareBracket());
+            Assert.IsTrue('['.IsSquareBracket());
+            Assert.IsTrue(']'.IsSquareBracket());
+            Assert.IsFalse('{'.IsSquareBracket());
+            Assert.IsFalse('}'.IsSquareBracket());
+        }
+    }
+}
---
+++ Tanpohp.Extensions/CharExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tanpohp.Extensions
+{
+    public static class CharExtension
+    {
+        /// <summary>
+        /// Checks whether given char is a circle, curly or square bracket.
+        /// </summary>
+        /// <param name="char">Char to check.</param>
+        /// <returns></returns>
+        public static bool IsBracket(this char @char)
+        {
+            return @char == '(' || @char == ')' || @char == '{' || @char == '}' || @char == '[' || @char == ']';
+        } 
+
+        /// <summary>
+        /// Checks whether give char is a curly bracket ("{" or"}").
+        /// </summary>
+        /// <param name="char">Char to check.</param>
+        /// <returns></returns>
+        public static bool IsCurlyBracket(this char @char)
+        {
+            return @char == '{' || @char == '}';
+        }
+
+        /// <summary>
+        /// Checks whether give char is a square bracket ("[" or"]").
+        /// </summary>
+        /// <param name="char">Char to check.</param>
+        /// <returns></returns>
+        public static bool IsSquareBracket(this char @char)
+        {
+            return @char == '[' || @char == ']';
+        }
+
+        /// <summary>
+        /// Checks whether give char is a circle bracket ("(" or")").
+        /// </summary>
+        /// <param name="char">Char to check.</param>
+        /// <returns></returns>
+        public static bool IsCircleBracket(this char @char)
+        {
+            return @char == '(' || @char == ')';
+        }
+    }
+}
