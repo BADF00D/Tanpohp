22ae2d775a843ceb034077cff2664c2508aee2f9
David <hichaeretaqua@web.de>
2014-10-04 20:06:24 +0200

    Added RandomExtension and its tests.

---
+++ Tanpohp.ExtensionTest/RandomExtensionTest.cs
@@ -0,0 +1,43 @@
+﻿#region usings
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    /// <summary>
+    /// This test uses Random class and therefor might fail if implementation of Random changed. 
+    /// </summary>
+    [TestFixture]
+    public class RandomExtensionTest
+    {
+        [Test]
+        public void RandomOfCollection()
+        {
+            var random = new Random(0);
+            var collection = new[] {0, 1, 2, 3, 4, 5};
+            var set = new HashSet<int>();
+            for (var i = 0; i < 10000; i++)
+            {
+                set.Add(random.RandomOf(collection));
+            }
+            Assert.AreEqual(6, set.Count);
+        }
+
+        [Test]
+        public void RandomOfParameter()
+        {
+            var random = new Random(0);
+            var set = new HashSet<int>();
+            for (var i = 0; i < 10000; i++)
+            {
+                set.Add(random.RandomOf(0, 1, 2, 3, 4, 5));
+            }
+            Assert.AreEqual(6, set.Count);
+        }
+    }
+}
---
+++ Tanpohp.Extensions/RandomExtension.cs
@@ -0,0 +1,36 @@
+﻿#region usings
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Tanpohp.Extensions
+{
+    public static class RandomExtension
+    {
+        /// <summary>
+        /// Returns a random item of given paramter.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="random">Random instance.</param>
+        /// <param name="paramter">Parameters a random one should be choosen off.</param>
+        /// <returns></returns>
+        public static T RandomOf<T>(this Random random, params T[] paramter)
+        {
+            return random.RandomOf((ICollection<T>)paramter);
+        }
+
+        /// <summary>
+        /// Returns a random item of given collection.
+        /// </summary>
+        /// <typeparam name="T">Generic parameter.</typeparam>
+        /// <param name="random">Random instance.</param>
+        /// <param name="collection">Collection a random item should be choosen off.</param>
+        /// <returns></returns>
+        public static T RandomOf<T>(this Random random, ICollection<T> collection)
+        {
+            return collection.ItemAt(random.Next(collection.Count));
+        }
+    }
+}
