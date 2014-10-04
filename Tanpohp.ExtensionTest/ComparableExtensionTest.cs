b69c7879ca8d718012ecf893a24f816768f21a82
David <hichaeretaqua@web.de>
2014-10-04 18:48:56 +0200

    Added ComparableExtension and its test.

---
+++ Tanpohp.ExtensionTest/ComparableExtensionTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class ComparableExtensionTest
+    {
+        [Test]
+        public void IsInRangeWithIntTest0()
+        {
+            Assert.IsTrue(0.InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithIntTest1()
+        {
+            Assert.IsTrue(1.InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithIntTest2()
+        {
+            Assert.IsFalse(2.InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithIntTest3()
+        {
+            Assert.IsFalse((-2).InRangeOf(-1, 1));
+        }
+
+        [Test]
+        public void IsInRangeWithDoubleTest0()
+        {
+            Assert.IsTrue((0d).InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithDoubleTest1()
+        {
+            Assert.IsTrue(1d.InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithDoubleTest2()
+        {
+            Assert.IsFalse(2d.InRangeOf(-1, 1));
+        }
+        [Test]
+        public void IsInRangeWithDoubleTest3()
+        {
+            Assert.IsFalse((-2d).InRangeOf(-1, 1));
+        }
+    }
+}
---
+++ Tanpohp.Extensions/ComparableExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Tanpohp.Extensions
+{
+    public static class ComparableExtension
+    {
+        /// <summary>
+        /// Determines weather item (e.g. a number) is in between lower and upper, or equals one of them.
+        /// </summary>
+        /// <typeparam name="T">Generic paramter that implements IComparable<T>.</typeparam>
+        /// <param name="actual">Actual value to test.</param>
+        /// <param name="lower">Lower bound.</param>
+        /// <param name="upper">Upper bound.</param>
+        /// <returns></returns>
+        public static bool InRangeOf<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return actual.CompareTo(lower) >= 0 && actual.CompareTo(upper) <= 0;
+        }
+
+        /// <summary>
+        /// Determines weather item (e.g. a number) is smaller then lower or bigger then upper.
+        /// </summary>
+        /// <typeparam name="T">Generic paramter that implements IComparable<T>.</typeparam>
+        /// <param name="actual">Actual value to test.</param>
+        /// <param name="lower">Lower bound.</param>
+        /// <param name="upper">Upper bound.</param>
+        /// <returns></returns>
+        public static bool NotInRangeOf<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return !InRangeOf(actual, lower, upper);
+        }
+    }
+}
