959b690164e504b05efc803751ade96ccfc575a1
David <hichaeretaqua@web.de>
2014-10-04 20:30:51 +0200

    Added methods to StringExtension.

--- Tanpohp.ExtensionTest/StringExtensionTest.cs
+++ Tanpohp.ExtensionTest/StringExtensionTest.cs
@@ -18,5 +18,33 @@ namespace Tanpohp.ExtensionTest
 
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void TryFindNthIndexOfTest1()
+        {
+            const string source = "0xxx0xxx0xxx0xxx0xxx0";
+            int index;
+            var hasResult = source.TryFindNthIndexOf("0", 2, out index);
+            Assert.IsTrue(hasResult);
+            Assert.AreEqual(8, index);
+        }
+
+        [Test]
+        public void TryFindNthIndexOfTest2()
+        {
+            const string source = "0xxx0xxx0xxx0xxx0xxx0";
+            int index;
+            var hasResult = source.TryFindNthIndexOf("0", 6, out index);
+            Assert.IsFalse(hasResult);
+        }
+
+        [Test]
+        public void TryFindNthIndexOfTest3()
+        {
+            const string source = "0xxx0xxx0xxx0xxx0xxx0";
+            int index;
+            var hasResult = source.TryFindNthIndexOf("1", 2, out index);
+            Assert.IsFalse(hasResult);
+        }
     }
 }
--- Tanpohp.Extensions/StringExtension.cs
+++ Tanpohp.Extensions/StringExtension.cs
@@ -15,5 +15,25 @@ namespace Tanpohp.Extensions
         {
             return string.Format(format, parameter);
         }
+
+        /// <summary>
+        /// This method tries to find the n-th occurance (zero based) of the given string match. 
+        /// </summary>
+        /// <param name="string">String to search in.</param>
+        /// <param name="match">String to find.</param>
+        /// <param name="occurance">Occurance the index should be searched.</param>
+        /// <param name="index">Resulting (zero based)index if return is true.</param>
+        /// <returns>True, if index could be found, false otherwise.</returns>
+        public static bool TryFindNthIndexOf(this string @string, string match, uint occurance, out int index)
+        {
+            index = 0;
+            var i = 1;
+            while(i <= occurance && (index = @string.IndexOf(match, index +1)) != -1)
+            {
+                if (i == occurance) return true;
+                i++;
+            }
+            return false;
+        }
     }
 }
