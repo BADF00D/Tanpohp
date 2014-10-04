22412bd6200e57dc202f422c9a94a221c24b3884
David <hichaeretaqua@web.de>
2014-10-04 19:08:01 +0200

    Added DateTimeExtension and its tests.

---
+++ Tanpohp.ExtensionTest/DateTimeExtensionTest.cs
@@ -0,0 +1,50 @@
+﻿#region usings
+
+using System;
+using NUnit.Framework;
+using Tanpohp.Extensions;
+
+#endregion
+
+namespace Tanpohp.ExtensionTest
+{
+    [TestFixture]
+    public class DateTimeExtensionTest
+    {
+        [Test]
+        public static void IsFutureTest0()
+        {
+            Assert.IsFalse(DateTime.Now.IsFuture());
+        }
+
+        [Test]
+        public static void IsFutureTest1()
+        {
+            Assert.IsTrue((new DateTime(3000, 1, 1)).IsFuture());
+        }
+
+        [Test]
+        public static void IsFutureTest2()
+        {
+            Assert.IsFalse((new DateTime(1000, 1, 1)).IsFuture());
+        }
+
+        [Test]
+        public static void IsPastTest0()
+        {
+            Assert.IsFalse(DateTime.Now.IsPast());
+        }
+
+        [Test]
+        public static void IsPastTest1()
+        {
+            Assert.IsFalse((new DateTime(3000, 1, 1)).IsPast());
+        }
+
+        [Test]
+        public static void IsPastTest2()
+        {
+            Assert.IsTrue((new DateTime(1000, 1, 1)).IsPast());
+        }
+    }
+}
---
+++ Tanpohp.Extensions/DateTimeExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tanpohp.Extensions
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// Determines whether date is in future from parameter pointOfView.
+        /// </summary>
+        /// <param name="date">DateTime that has to be checked.</param>
+        /// <param name="pointOfView">Point of view used to determine if date is in future.</param>
+        /// <returns></returns>
+        public static bool IsFuture(this DateTime date, DateTime pointOfView)
+        {
+            return date.Date > pointOfView.Date;
+        }
+
+        /// <summary>
+        /// Determines whether date is in future from now.
+        /// </summary>
+        /// <param name="date">DateTime that has to be checked.</param>
+        public static bool IsFuture(this DateTime date)
+        {
+            return date.IsFuture(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Determines whether date is in past from parameter pointOfView.
+        /// </summary>
+        /// <param name="date">DateTime that has to be checked.</param>
+        /// <param name="pointOfView">Point of view used to determine if date is in future.</param>
+        public static bool IsPast(this DateTime date, DateTime pointOfView)
+        {
+            return date.Date < pointOfView.Date;
+        }
+
+        /// <summary>
+        /// Determines whether date is in past from now.
+        /// </summary>
+        /// <param name="date">DateTime that has to be checked.</param>
+        public static bool IsPast(this DateTime date)
+        {
+            return date.IsPast(DateTime.Now);
+        }
+    }
+}
