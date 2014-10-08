a905f3351b6dd3cfb77d9368c74e08fa7550b81f
David <hichaeretaqua@web.de>
2014-10-08 16:13:53 +0200

    Added SquareRootNode and its tests.

---
+++ Tanpohp.Mathematics/Calculation/UnaryOperations/SquareRootNode.cs
@@ -0,0 +1,21 @@
+﻿#region usings
+
+using System;
+using Tanpohp.Annotations.Resharper;
+
+#endregion
+
+namespace Tanpohp.Mathematics.Calculation.UnaryOperations
+{
+    public class SquareRootNode : UnaryOperationNode
+    {
+        public SquareRootNode([NotNull] ICalculationNode node) : base(node)
+        {
+        }
+
+        public override double Evaluate()
+        {
+            return Math.Sqrt(Node.Evaluate());
+        }
+    }
+}
