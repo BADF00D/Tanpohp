e3b9fa5bd1e7879c79aae2814f0a9aea127c851b
David <hichaeretaqua@web.de>
2014-10-20 10:45:53 +0200

    Added ACanAlwaysExcecuteCommand.

---
+++ Tanpohp.Wpf/Commands/ACanAlwaysExcecuteCommand.cs
@@ -0,0 +1,25 @@
+﻿#region usings
+
+using System;
+using System.Windows.Input;
+
+#endregion
+
+namespace Tanpohp.Wpf.Commands
+{
+    public abstract class ACanAlwaysExcecuteCommand : ICommand
+    {
+        #region ICommand Members
+
+        public abstract void Execute(object parameter);
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        #endregion
+    }
+}
