187ab0de8a7d964d6077d363524a8c9adcf9aa90
David <hichaeretaqua@web.de>
2014-10-24 08:58:25 +0200

    Added tests for ACanAlwaysExcecuteCommand class.

---
+++ Tanpohp.WpfTest/Commands/ACanAlwaysExcecuteCommandTest.cs
@@ -0,0 +1,46 @@
+﻿#region usings
+
+using System;
+using NUnit.Framework;
+using Tanpohp.Wpf.Commands;
+
+#endregion
+
+namespace Tanpohp.WpfTest.Commands
+{
+    [TestFixture]
+    public class ACanAlwaysExcecuteCommandTest
+    {
+        private class DummyACanAlwaysExcecuteCommand : ACanAlwaysExcecuteCommand
+        {
+            public override void Execute(object parameter)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Test]
+        public void CanExcecuteWithInteger()
+        {
+            var dummy = new DummyACanAlwaysExcecuteCommand();
+
+            Assert.IsTrue(dummy.CanExecute(5));
+        }
+
+        [Test]
+        public void CanExcecuteWithNull()
+        {
+            var dummy = new DummyACanAlwaysExcecuteCommand();
+
+            Assert.IsTrue(dummy.CanExecute(null));
+        }
+
+        [Test]
+        public void CanExcecuteWithObject()
+        {
+            var dummy = new DummyACanAlwaysExcecuteCommand();
+
+            Assert.IsTrue(dummy.CanExecute(new object()));
+        }
+    }
+}
