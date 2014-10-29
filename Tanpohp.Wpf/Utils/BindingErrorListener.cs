8cff42852cc12b46c9f371cc6961d00e48ab415a
David <hichaeretaqua@web.de>
2014-10-29 09:54:28 +0100

    Added BindingErrorListener to Tanpohp.Wpf.

---
+++ Tanpohp.Wpf/Utils/BindingErrorListener.cs
@@ -0,0 +1,42 @@
+﻿#region usings
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+#endregion
+
+namespace Tanpohp.Wpf.Utils
+{
+    /// <summary>
+    /// Listens to binding errors as inspired from here: http://stackoverflow.com/questions/4225867/how-can-i-turn-binding-errors-into-runtime-exceptions
+    /// </summary>
+    public class BindingErrorListener : TraceListener
+    {
+        private Action<string> _logAction;
+
+        public static void Listen(Action<string> logAction)
+        {
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new BindingErrorListener {_logAction = logAction});
+        }
+
+        public static void HandleErrorsAsExceptions()
+        {
+            Listen(message => { throw new Exception(message); });
+        }
+
+        public static void DisplayErrorsInMessageBox()
+        {
+            Listen(message => MessageBox.Show(message));
+        }
+
+        public override void Write(string message)
+        {
+        }
+
+        public override void WriteLine(string message)
+        {
+            _logAction(message);
+        }
+    }
+}
