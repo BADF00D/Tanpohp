125c0f2491dc282d3a8dd6e80916e4f39e0b6c54
David <hichaeretaqua@web.de>
2014-12-23 12:20:58 +0100

    Restructured some test.

--- Tanpohp.MathematicsTest/Extension/Vector3Extension.cs
+++ Tanpohp.MathematicsTest/Extension/Vector3Extension.cs
@@ -5,18 +5,18 @@
 
     public static class Vector3Extension
     {
-        public static void AssertAreEqual(this Vector3 one, Vector3 two, double delta = 0.0001)
+        public static void AssertAreEqual(this Vector3 actual, Vector3 expected, double delta = 0.0001)
         {
-            Assert.AreEqual(one.X, two.X, delta, "X");
-            Assert.AreEqual(one.Y, two.Y, delta, "Y");
-            Assert.AreEqual(one.Z, two.Z, delta, "Z");
+            Assert.AreEqual(expected.X, actual.X, delta, "X");
+            Assert.AreEqual(expected.Y, actual.Y, delta, "Y");
+            Assert.AreEqual(expected.Z, actual.Z, delta, "Z");
         }
 
-        public static void AssertAreEqual(this Vector3 one, Vector3 two, Vector3 delta)
+        public static void AssertAreEqual(this Vector3 actual, Vector3 expected, Vector3 delta)
         {
-            Assert.AreEqual(one.X, two.X, delta.X, "X");
-            Assert.AreEqual(one.Y, two.Y, delta.Y, "Y");
-            Assert.AreEqual(one.Z, two.Z, delta.Z, "Z");
+            Assert.AreEqual(expected.X, actual.X, delta.X, "X");
+            Assert.AreEqual(expected.Y, actual.Y, delta.Y, "Y");
+            Assert.AreEqual(expected.Z, actual.Z, delta.Z, "Z");
         }
     }
 }
--- Tanpohp.MathematicsTest/Spline/SplinePathGeneratorTest.cs
+++ Tanpohp.MathematicsTest/Spline/SplinePathGeneratorTest.cs
@@ -43,15 +43,15 @@
         {
             var point0 = new Vector3(0, 0, 0);
             var point1 = new Vector3(1, 0, 0);
-            var point2 = new Vector3(9, 0, 0);
-            var point3 = new Vector3(10, 0, 0);
+            var point2 = new Vector3(10, 0, 0);
+            var point3 = new Vector3(11, 0, 0);
             var controlPoints = new List<Vector3> {point0, point1, point2, point3};
 
             Expect.MethodCall(() => _cubicSplineInterpolator.Interpolate(.25, 0.5, point0, point1, point2, point3));
             Expect.MethodCall(() => _cubicSplineInterpolator.Interpolate(.5, 0.5, point0, point1, point2, point3));
             Expect.MethodCall(() => _cubicSplineInterpolator.Interpolate(.75, 0.5, point0, point1, point2, point3));
 
-            var result = _splinePathGenerator.Generate(controlPoints, 2, 0.5);
+            var result = _splinePathGenerator.Generate(controlPoints, 2.25, 0.5);
             Assert.AreEqual(result.Count, 1 + 3 + 1); //point1 + 3 interpolated points + point2
             result[0].AssertAreEqual(point1);
             result[4].AssertAreEqual(point2);
