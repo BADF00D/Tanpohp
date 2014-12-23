08292601872b8e080f4a2fcbcabd094d04125f2d
David <hichaeretaqua@web.de>
2014-12-23 11:40:17 +0100

    Added comments ISplinePathGenerator.

--- Tanpohp.Mathematics/Spline/ISplinePathGenerator.cs
+++ Tanpohp.Mathematics/Spline/ISplinePathGenerator.cs
@@ -4,6 +4,13 @@
 
     public interface ISplinePathGenerator
     {
+        /// <summary>
+        /// Generates a spline between given control points.
+        /// </summary>
+        /// <param name="controlPoints">List of control point. First and last are not part of resulting spline.</param>
+        /// <param name="lengthPerSegment">Approximate lenth between two resulting interpolated points.</param>
+        /// <param name="splineParameter">Spline parameter passed to spline interpolation algorithm.</param>
+        /// <returns>List of Vector3 that thats at second control point and an stops at next to last control point.</returns>
         IList<Vector3> Generate(IList<Vector3> controlPoints, double lengthPerSegment, double splineParameter);
     }
 }
